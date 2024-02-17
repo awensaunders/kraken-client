353e4aac09ed441a2b14259dd7f97d79dc6006c4
Awen Saunders <me@awen.io>
2024-02-17 01:12:53 +0000

    Change some loglevels

--- energy_api/main.py
+++ energy_api/main.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import logging.config
 import os
 from graphql_client import Client
 from energy_api.kraken_rest_client import KrakenRestClient
@@ -25,22 +26,22 @@ async def main():
         raise Exception("No credentials found")
 
     logger.info("Logged In Successfully")
-    logger.info(f"Token: {token}")
+    logger.debug(f"Token: {token}")
     token_decoded = jwt.decode(token, options={"verify_signature": False})
     exp = token_decoded["exp"]
     expiry = datetime.datetime.fromtimestamp(exp, tz=datetime.UTC)
-    logger.info(f"Token Expiry: {expiry}")
+    logger.debug(f"Token Expiry: {expiry}")
     refresh_expiry = datetime.datetime.fromtimestamp(
         res.refresh_expires_in, tz=datetime.UTC
     )
-    logger.info(f"Refresh Token Expiry: {refresh_expiry}")
+    logger.debug(f"Refresh Token Expiry: {refresh_expiry}")
 
     headers = {"Authorization": f"{token}"}
     authed_client = Client(endpoint, headers=headers)
     res = await authed_client.get_account_info()
-    logger.info(f"Logged in as {res.family_name} {res.given_name}")
+    logger.info(f"Logged in as {res.given_name} {res.family_name}")
     if res.live_secret_key is not None:
-        logger.info(f"Live Secret Key: {res.live_secret_key}")
+        logger.debug(f"Live Secret Key: {res.live_secret_key}")
     else:
         logger.info("No live secret key found")
         logger.info("Creating a new live secret key")
@@ -48,9 +49,10 @@ async def main():
         logger.info(f"Generated new secret key: {res.live_secret_key}")
     res = await authed_client.get_account_info()
     accounts = res.accounts
-    logger.info(f"Account Info: {res.model_dump_json()}")
+    logger.debug(f"Account Info: {res.model_dump_json()}")
     rc = KrakenRestClient("https://api.eonnext-kraken.energy/v1", headers)
     for account in accounts:
+        logger.info(f"Getting detailed account information for {account.number}")
         gql_acc_details = await authed_client.account_agreement_and_meter_details(
             account.number
         )
@@ -59,11 +61,10 @@ async def main():
         mpan = active_agreement.meter_point.mpan
         msn = active_agreement.meter_point.meters[0].serial_number
         unit_rate = active_agreement.tariff.unit_rate
-        logger.info(f"Getting account information for {account.number}")
         logger.info(f"Standing Charge: {standing_charge}")
         logger.info(f"Unit Rate: {unit_rate}")
         res = await rc.get_account_information(account.number)
-        logger.info(f"Account Information: {res.model_dump_json()}")
+        logger.debug(f"Account Information: {res.model_dump_json()}")
         logger.info(f"Gathering consumption data for msn: {msn}")
         consumption = rc.get_consumption(mpan, msn, granularity="day")
         async for entry in consumption:
